673c50e82ddc99e89714e302318534843428365c
eberesquivel <eberesquivel@gmail.com>
2020-07-01 00:12:18 -0500

    cambios

--- apiTest_app.py
+++ apiTest_app.py
@@ -2,10 +2,20 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask
+from flask_sqlalquemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
 
+
+""" Inicia la aplicacion """
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+def get():
+    return  jsonify({'msg':'Hola Mundito'})
 
+
+""" Corre el servidor """
+
+if  __name__ == '__main__':
+    app.run(debug=True)
